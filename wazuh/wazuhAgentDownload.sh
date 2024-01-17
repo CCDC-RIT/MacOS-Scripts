288debba16515b4d6089a78f97aa80f4b505655d
Hal Williams <90720910+hfw8271@users.noreply.github.com>
2024-01-17 10:45:59 -0500

    user input for wazuh manager

--- wazuh/wazuhAgentDownload.sh
+++ wazuh/wazuhAgentDownload.sh
@@ -5,6 +5,9 @@ if [ "$EUID" -ne 0 ]; then
   exit
 fi
 
+echo "Wazuh Manager: "
+read wazuhManager
+
 if [ ../intel/versionDetection.sh -ne "Cope" ]; then
     version=$(../intel/versionDetection.sh | awk '{print $4}')
 else
@@ -12,10 +15,10 @@ else
 fi
 archType=($../intel/archType.sh)
 if [ "$version" -ne "Cope" && $"archType" == "x86_64" ]; then
-    echo "WAZUH_MANGER='IDK'" > /tmp/wazuh_envs && installer -pkg wazuh-agent-4.7.2-1.intel64.pkg -target /
+    echo -e "WAZUH_MANGER='$wazuhManager'" > /tmp/wazuh_envs && installer -pkg wazuh-agent-4.7.2-1.intel64.pkg -target /
 elif [ "$version" == "Big Sur" || "$version" == "Monterey" || "$version" == "Ventura" || "$version" == "Sonoma" ]; then
     if [ "$archType" == "arm64"]; then
-        echo "WAZUH_MANAGER='10.0.0.2'" > /tmp/wazuh_envs && installer -pkg wazuh-agent-4.7.2-1.arm64.pkg -target /
+        echo -e "WAZUH_MANAGER='$wazuhManager'" > /tmp/wazuh_envs && installer -pkg wazuh-agent-4.7.2-1.arm64.pkg -target /
     fi
 else
     echo "This is to old we F'ed"
